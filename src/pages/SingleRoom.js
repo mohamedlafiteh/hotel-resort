1b60accd324a93620f56900b482ecfd2c165ab7e
mohamedlafiteh <m_lafiteh@yahoo.com>
2019-09-05 12:44:38 +0100

    Create component and set up files for the project

---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class SingleRoom extends Component {
+  render() {
+    return <div>hello from single room page</div>;
+  }
+}
+
+export default SingleRoom;
